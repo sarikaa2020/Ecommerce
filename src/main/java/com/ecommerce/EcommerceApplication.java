736c357f0688c9ea69b1fe4e6416c8c0f362eb46
sarikaa shree.v <kit27.am46@gmail.com>
2025-09-29 15:36:23 +0530

    Initial commit with signup page

---
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -0,0 +1,10 @@
+package main.java.com.ecommerce;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+@SpringBootApplication
+
+public class EcommerceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApplication.class, args);
+    }
+}
